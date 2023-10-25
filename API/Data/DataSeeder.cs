2ce4a5bcc0820356e3d7b9f2d28bf2f2c432a120
Madridista91 <a.jangelovski@yahoo.com>
2023-10-25 15:09:20 +0200

    Add project files.

---
+++ API/Data/DataSeeder.cs
@@ -0,0 +1,44 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Data
+{
+    public class DataSeeder
+    {
+  
+        public static void Seed(WebApplicationBuilder builder)
+        {
+            using(var serviceScope = builder.Services.BuildServiceProvider().GetRequiredService<IServiceScopeFactory>().CreateScope()) 
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
+                
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(new List<AppUser>()
+                    {
+                        new AppUser()
+                        {
+                            
+                            UserName = "JohnDoe",
+                        },
+                        new AppUser()
+                        {
+
+                            UserName = "ElisabethRobbie",
+                        },
+                        new AppUser()
+                        {
+                            UserName = "MaXMustermann"
+                        },
+                        new AppUser()
+                        {
+                            UserName = "FranciscaChantel"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
