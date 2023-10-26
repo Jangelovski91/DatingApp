e94ce55efda4bbbef9c4e11664ba2a2d67dfb260
Madridista91 <a.jangelovski@yahoo.com>
2023-10-26 23:17:41 +0200

    creating Client App

--- API/Program.cs
+++ API/Program.cs
@@ -16,9 +16,11 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 DataSeeder.Seed(builder);
-
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
